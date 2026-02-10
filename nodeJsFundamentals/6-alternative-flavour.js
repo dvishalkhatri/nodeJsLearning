6c0afd117c5bee780ca8a4ac63498944f38a8885
Vishal Kumar Khatri <44779843+dvishalkhatri@users.noreply.github.com>
2026-02-10 22:08:00 +0530

    Alternative Flavour

---
+++ nodeJsFundamentals/6-alternative-flavour.js
@@ -0,0 +1,5 @@
+module.exports.items = ['item1', 'item2'];
+const person = {
+  name: 'Victor',
+};
+module.exports.singlePerson = person;
--- nodeJsFundamentals/app.js
+++ nodeJsFundamentals/app.js
@@ -2,6 +2,8 @@
 // Modules - Encapsulated Code (only share minimum)
 const names = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavour');
+console.log(data);
 
 sayHi(names.vishal);
 sayHi(names.vinay);

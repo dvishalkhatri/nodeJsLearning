2a74f9b460257168867294b986d525e387b3d289
Vishal Kumar Khatri <44779843+dvishalkhatri@users.noreply.github.com>
2026-02-14 20:57:27 +0530

    File async module

---
+++ nodeJsFundamentals/11-fs-async.js
@@ -0,0 +1,26 @@
+const { readfile, writeFile, readFile, read } = require('fs');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          return;
+        }
+        console.log(result);
+      },
+    );
+  });
+});

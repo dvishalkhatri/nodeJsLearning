43f10f9c670c3665ef511dd28c787a23e180c3ac
Vishal Kumar Khatri <44779843+dvishalkhatri@users.noreply.github.com>
2026-02-14 20:13:20 +0530

    OS Module

--- nodeJsFundamentals/app.js
+++ nodeJsFundamentals/app.js
@@ -1,10 +1,16 @@
-// CommonJs, every file is module (by default)
-// Modules - Encapsulated Code (only share minimum)
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavour');
-require('./7-mind-grenade');
+const os = require('os');
 
-sayHi(names.vishal);
-sayHi(names.vinay);
-sayHi('Vicky');
+// info about the current user
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
